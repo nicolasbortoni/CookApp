88179dc38f14bda731a468b1497116fc198e0243
Nicolas Bortoni <nicobortoni@frba.utn.edu.ar>
2022-04-26 22:43:57 +0200

    Navegación addFragment agregada

--- app/src/main/java/com/utn/cookapp/fragments/RecyclerViewFragment.kt
+++ app/src/main/java/com/utn/cookapp/fragments/RecyclerViewFragment.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.lifecycle.ViewModel
+import androidx.navigation.findNavController
 import com.utn.cookapp.R
 import com.utn.cookapp.viewmodels.RecyclerViewViewModel
 
@@ -16,12 +19,24 @@ class RecyclerViewFragment : Fragment() {
     }
 
     private lateinit var viewModel: RecyclerViewViewModel
+    private lateinit var v : View
+    private lateinit var addButton : Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.recycler_view_fragment, container, false)
+        v = inflater.inflate(R.layout.recycler_view_fragment, container, false)
+        addButton = v.findViewById(R.id.addBtn)
+        return v
+    }
+
+    override fun onStart() {
+        super.onStart()
+        addButton.setOnClickListener {
+            val action = RecyclerViewFragmentDirections.actionRecyclerViewFragmentToAddFragment()
+            v.findNavController().navigate(action)
+        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
