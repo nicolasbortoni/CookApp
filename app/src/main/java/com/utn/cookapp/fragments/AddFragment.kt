f08c850d27a70467297effc61d2f135b0369f4f9
Nicolas Bortoni <nicobortoni@frba.utn.edu.ar>
2022-05-03 03:20:40 +0200

    Mejora en el ingreso de texto del recipe

--- app/src/main/java/com/utn/cookapp/fragments/AddFragment.kt
+++ app/src/main/java/com/utn/cookapp/fragments/AddFragment.kt
@@ -72,7 +72,7 @@ class AddFragment : Fragment() {
             recipeName.setText(recipeToEdit.name)
             author.setText(recipeToEdit.author)
             image.setText(recipeToEdit.image)
-            addBtn.setText("Edit")
+            addBtn.setText("Accept")
         }
         addBtn.setOnClickListener {
             if (recipeToEdit.id==-1) {
